5441186cefa5353ec5e866e2ba9614061c648a72
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-11-15 00:26:34 +0300

    feat: implement getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -294,8 +294,8 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return [...`${num}`].reduce((acc, curr) => Number(acc) + Number(curr), 0);
 }
 
 /**
