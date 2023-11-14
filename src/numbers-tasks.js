4bb8cbbdf3d2b443a1bed2df5e2c82d47fcf7b27
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-11-15 00:19:27 +0300

    feat: implement getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -275,8 +275,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let output = 0;
+  for (let i = 1; i <= n; i += 1) {
+    output += i;
+  }
+  return output;
 }
 
 /**
